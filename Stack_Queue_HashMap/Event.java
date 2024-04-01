3404c2635224d652e5ddece5cadcdaf9086cc80b
ngcsch <x00217230227@njit.edu.cn>
2024-04-01 19:48:58 +0800

    Stack, Queue and HashMap practice exercise submission

---
+++ Stack_Queue_HashMap/Event.java
@@ -0,0 +1,181 @@
+package Stack_Queue_HashMap;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Event {
+
+    private String eventID;
+    private String eventName;
+    private String eventVenue;
+    private Date eventDate;
+    private ArrayList<String> eventAttendees;
+
+    // constructor
+    public Event() {
+        this.eventAttendees = new ArrayList<>();
+    }
+
+    // Constructor, with arguments
+    public Event(String eventID, String eventName, String eventVenue, Date eventDate) {
+        this.eventID = eventID;
+        this.eventName = eventName;
+        this.eventVenue = eventVenue;
+        this.eventDate = eventDate;
+    }
+
+    // Method for obtaining the event ID
+    public String getEventID() {
+        return eventID;
+    }
+
+    // Method for setting the event ID
+    public void setEventID(String eventID) {
+        this.eventID = eventID;
+    }
+
+    // Method to get the event name
+    public String getEventName() {
+        return eventName;
+    }
+
+    // Method for setting the event name
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    // Method of getting event location
+    public String getEventVenue() {
+        return eventVenue;
+    }
+
+    public void setEventVenue(String eventVenue) {
+        this.eventVenue = eventVenue;
+    }
+
+    // Method to get the event date
+    public Date getEventDate() {
+        return eventDate;
+    }
+
+    // A way to set the event date
+    public void setEventDate(Date eventDate) {
+        this.eventDate = eventDate;
+    }
+
+    // Method to get a list of event participants
+    public ArrayList<String> getEventAttendees() {
+        return eventAttendees;
+    }
+
+    // Methods for adding event participants
+    public void addEventAttendee(String attendee) {
+        eventAttendees.add(attendee);
+    }
+
+    // Method for removing event participants
+    public void removeEventAttendee(String attendee) {
+        eventAttendees.remove(attendee);
+    }
+
+    // Methods for updating event participants
+    public void updateEventAttendee(String oldAttendee, String newAttendee) {
+        int index = eventAttendees.indexOf(oldAttendee);
+        if (index != -1) {
+            eventAttendees.set(index, newAttendee);
+        }
+    }
+
+    // Ways to find event participants
+    public boolean findEventAttendee(String attendee) {
+        return eventAttendees.contains(attendee);
+    }
+
+    // A way to organize events
+    public void organizeEvent() {
+        System.out.println("event ID：" + eventID);
+        System.out.println("eventName：" + eventName);
+        System.out.println("eventVenue：" + eventVenue);
+        System.out.println("eventDate：" + eventDate);
+        System.out.println("eventAttendees：" + eventAttendees.size());
+    }
+
+    // toString Method to print object information
+    @Override
+    public String toString() {
+        return "Event{" +
+                "eventID='" + eventID + '\'' +
+                ", eventName='" + eventName + '\'' +
+                ", eventVenue='" + eventVenue + '\'' +
+                ", eventDate=" + eventDate +
+                ", eventAttendees=" + eventAttendees +
+                '}';
+    }
+
+    @SuppressWarnings("deprecation")
+    public static void main(String[] args) {
+        Event event = new Event();
+
+        try (// Gets the event information entered by the user
+                Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Please enter an event ID：");
+            event.setEventID(scanner.nextLine());
+
+            System.out.print("Please enter an eventName：");
+            event.setEventName(scanner.nextLine());
+
+            System.out.print("Please enter an eventVenue：");
+            event.setEventVenue(scanner.nextLine());
+
+            System.out.print("Please enter the event date（yyyy-MM-dd）：");
+            String date = scanner.nextLine();
+            try {
+                event.setEventDate(new Date(date));
+            } catch (Exception e) {
+                System.out.println("Invalid date format, please re-enter.");
+                return;
+            }
+
+            // Allows users to add, remove, update, and find participants
+            while (true) {
+                System.out.println("1. Add a participant");
+                System.out.println("2. Remove participant");
+                System.out.println("3. Update participant");
+                System.out.println("4. Find a participant");
+                System.out.println("5. over");
+
+                int choice = scanner.nextInt();
+                scanner.nextLine(); // Consume newlines in the input buffer
+
+                if (choice == 1) {
+                    System.out.print("Please enter the name of the participant you want to add：");
+                    String attendee = scanner.nextLine();
+                    event.addEventAttendee(attendee);
+                } else if (choice == 2) {
+                    System.out.print("Please enter the name of the participant you want to remove：");
+                    String attendeeToRemove = scanner.nextLine();
+                    event.removeEventAttendee(attendeeToRemove);
+                } else if (choice == 3) {
+                    System.out.print("Please enter the old participant name that you want to update：");
+                    String oldAttendee = scanner.nextLine();
+                    System.out.print("Please enter the name of the new participant to update：");
+                    String newAttendee = scanner.nextLine();
+                    event.updateEventAttendee(oldAttendee, newAttendee);
+                } else if (choice == 4) {
+                    System.out.print("Please enter the name of the participant you are looking for：");
+                    String attendeeToFind = scanner.nextLine();
+                    if (event.findEventAttendee(attendeeToFind)) {
+                        System.out.println("Participants found " + attendeeToFind);
+                    } else {
+                        System.out.println("Participant not found " + attendeeToFind);
+                    }
+                } else if (choice == 5) {
+                    break;
+                }
+            }
+        }
+        // Organize events and print results
+        event.organizeEvent();
+    }
+}
--- Stack_Queue_HashMap/Main.java
+++ Stack_Queue_HashMap/Main.java
@@ -1,34 +1,63 @@
 package Stack_Queue_HashMap;
 
-import java.util.Stack;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String exp = "{([])}";
-        System.out.println(checkBalancedParentheses(exp));
-    }
+        Scanner scanner = new Scanner(System.in);
 
-    public static boolean checkBalancedParentheses(String exp) {
-        Stack<Character> stack = new Stack<Character>();
-        for (int i = 0; i < exp.length(); i++) {
-            char c = exp.charAt(i);
-            if (c == '(' || c == '[' || c == '{') {
-                stack.push(c);
-            } else if (c == ')' || c == ']' || c == '}') {
-                if (stack.empty()) {
-                    return false;
-                } else {
-                    char topChar = stack.peek();
-                    if (c == ')' && topChar!= '('
-                            || c == ']' && topChar!= '['
-                            || c == '}' && topChar!= '{') {
-                        return false;
-                    } else {
-                        stack.pop();
-                    }
-                }
+        int num, flag = 0;
+        Event event = new Event();
+        System.out.println("Enter the event ID: ");
+        String eventID = scanner.nextLine();
+        event.setEventID(eventID);
+        System.out.println("Enter the event name: ");
+        String eventName = scanner.nextLine();
+        event.setEventName(eventName);
+        System.out.println("Enter the event venue: ");
+        String eventVenue = scanner.nextLine();
+        event.setEventVenue(eventVenue);
+
+        do {
+            System.out.println("Enter the event date(format: yyyy/MM/dd):");
+            String eventDate = scanner.next();
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+            try {
+                Date date = dateFormat.parse(eventDate);
+                event.setEventDate(date);
+                flag = 1;
+            } catch (ParseException e) {
+                System.out.println("Invalid date format. Please enter date in the format yyyy/MM/dd.");
             }
+        } while (flag == 0);
+        System.out.println("Enter the number of attendees: ");
+        num = scanner.nextInt();
+        scanner.nextLine();
+        for (int i = 0; i < num; i++) {
+            System.out.println("Enter the name of attendee " + (i + 1) + ": ");
+            String attendee = scanner.nextLine();
+            System.out.println("Enter the email of attendee " + (i + 1) + ": ");
+            String email = scanner.nextLine();
+            System.out.println("Enter the phone of attendee " + (i + 1) + ": ");
+            String phone = scanner.nextLine();
+            System.out.println("Enter the address of attendee " + (i + 1) + ": ");
+            String address = scanner.nextLine();
+            System.out.println("Enter the seat number of attendee " + (i + 1) + ": ");
+            String seatNo = scanner.nextLine();
+            event.getEventAttendees().addAll(new Attendee(attendee, email, phone, address, seatNo));
         }
-        return stack.empty();
+        event.organizeEvent();
+        System.out.println("The details of the event are: ");
+        System.out.println("Event ID: " + event.getEventID());
+        System.out.println("Event Name: " + event.getEventName());
+        System.out.println("Event Venue: " + event.getEventVenue());
+        System.out.println("Event Date: " + event.getEventDate());
+        System.out.println("Event Attendees: ");
+        for (String attendee :  event.getEventAttendees()) {
+            System.out.println(attendee);
+        }
     }
 }
